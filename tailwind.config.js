13f2e44bc9fab087f684674f513b693a888b3150
Hendrik <hendrikp49@gmail.com>
2024-09-15 21:14:53 +0800

    initial project

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        "huko-primary": "#181818",
+        "huko-secondary": "#E6E6E6",
+        "huko-1": "#ADD8E6",
+        "huko-2": "#00CED1",
+        "huko-3": "#4682D4",
+      },
+    },
+  },
+  plugins: [],
+};
